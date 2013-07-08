c6b8766edfe090a7b749aa4c8f03efb0fe22e08d
Jose Diaz-Gonzalez <myaccounts@savant.be>
2013-07-07 23:04:03 -0400

    general cleanup

--- Controller/Component/CsvViewComponent.php
+++ Controller/Component/CsvViewComponent.php
@@ -29,15 +29,14 @@ class CsvViewComponent extends Component {
  * Prepares an array of all unique Hash::extract() compatible paths,
  * from the results of a model find('all') call.
  *
- * @param  array $data          the results of a model find('all') call.
- * @param  array  $excludePaths [description]
- * @return array                an array of Hash::extract() compatible paths
+ * @param array $data the results of a model find('all') call.
+ * @param array $excludePaths [description]
+ * @return array an array of Hash::extract() compatible paths
  */
 	public function prepareExtractFromFindResults($data, $excludePaths = array()) {
 		$extract = array();
-		// Go over every row in case some have keys missing
 		foreach ($data as $numericKey => $row) {
-			$this->_addUniquePaths($row, $excludePaths, $extract);
+			$this->_addUniquePaths($extract, $row, $excludePaths);
 		}
 		return $extract;
 	}
@@ -46,25 +45,27 @@ class CsvViewComponent extends Component {
  * Recursively searches a single row from the results of a model find('all') and
  * adds all unique Hash::extract() compatible paths to $extract
  *
- * @param array $dataRow       a single row from the results of a model find('all')
- * @param array $excludePaths  an array of Hash::extract() compatible paths to be excluded
- * @param array $extract       reference to the array containing all unique paths
- * @param string $parentPath   Hash::extract() compatible string of all paths up until this point (for deep nested arrays)
+ * @param array $dataRow a single row from the results of a model find('all')
+ * @param array $excludePaths an array of Hash::extract() compatible paths to be excluded
+ * @param array $extract reference to the array containing all unique paths
+ * @param string $parentPath Hash::extract() compatible string of all paths up until this point (for deep nested arrays)
  * @return void
  */
-	protected function _addUniquePaths($dataRow, $excludePaths, &$extract, $parentPath = '') {
+	protected function _addUniquePaths(&$extract, $dataRow, $excludePaths, $parentPath = '') {
 		foreach ($dataRow as $key => $value) {
-			$fullPath = ($parentPath !== '')? $parentPath . '.' . $key : $key;
+			$fullPath = $key;
+			if ($parentPath !== '') {
+				$fullPath = $parentPath . '.' . $key;
+			}
+
 			if (is_numeric($key)) {
-				// ignore it - it's a nested hasMany association
-			} else {
-				if (is_array($value)) {
-					$this->_addUniquePaths($value, $excludePaths, $extract, $fullPath);
-				} else {
-					if (array_search($fullPath, $extract) === false && array_search($fullPath, $excludePaths) === false) {
-						$extract[] = $fullPath;
-					}
-				}
+				continue;
+			}
+
+			if (is_array($value)) {
+				$this->_addUniquePaths($extract, $value, $excludePaths, $fullPath);
+			} elseif (array_search($fullPath, $extract) === false && array_search($fullPath, $excludePaths) === false) {
+				$extract[] = $fullPath;
 			}
 		}
 	}
@@ -72,9 +73,9 @@ class CsvViewComponent extends Component {
 /**
  * Prepare an array of user-friendly column titles based on an array of Hash::extract() compatible paths
  *
- * @param  array $extract        an array of Hash::extract() compatible paths
- * @param  array  $customHeaders array of 'Hash.Path' => 'Custom Title' pairs, to override default generated titles
- * @return array                 an array of user-friendly headers, matching the passed in $extract array
+ * @param array $extract an array of Hash::extract() compatible paths
+ * @param array $customHeaders array of 'Hash.Path' => 'Custom Title' pairs, to override default generated titles
+ * @return array an array of user-friendly headers, matching the passed in $extract array
  */
 	public function prepareHeaderFromExtract($extract, $customHeaders = array()) {
 		$header = array();
@@ -84,7 +85,7 @@ class CsvViewComponent extends Component {
 			} else {
 				$pathParts = explode('.', $fullPath);
 				$model = $pathParts[count($pathParts) - 2];
-				$model = preg_replace('/(?<! )(?<!^)[A-Z]/',' $0', $model); // Add space before capitals, excluding first capital - eg. MyModel becomes My Model
+				$model = preg_replace('/(?<! )(?<!^)[A-Z]/',' $0', $model);
 
 				$column = $pathParts[count($pathParts) - 1];
 				$column = str_replace('_', ' ', $column);
@@ -100,10 +101,10 @@ class CsvViewComponent extends Component {
 /**
  * Quickly export the results of a model find('all') call with a single line of code.
  *
- * @param  array $data            the results of a model find('all') call.
- * @param  array $excludePaths    an array of Hash::extract() compatible paths to be excluded
- * @param  array $customHeaders   array of 'Hash.Path' => 'Custom Title' pairs, to override default generated titles
- * @param  boolean $includeHeader if true, a header will be included in the exported CSV.
+ * @param array $data the results of a model find('all') call.
+ * @param array $excludePaths an array of Hash::extract() compatible paths to be excluded
+ * @param array $customHeaders array of 'Hash.Path' => 'Custom Title' pairs, to override default generated titles
+ * @param boolean $includeHeader if true, a header will be included in the exported CSV.
  * @return void
  */
 	public function quickExport($data, $excludePaths = array(), $customHeaders = array(), $includeHeader = true) {
