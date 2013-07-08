d9a25e4fe8627536ee7f309cfcb01fb77f0bb8ab
Jose Diaz-Gonzalez <myaccounts@savant.be>
2013-07-07 23:10:01 -0400

    Added AllCsvViewTest class

---
+++ Test/Case/AllCsvViewTest.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * All CsvView plugin tests
+ *
+ * @package       CsvView.Test.Case
+ */
+class AllCsvViewTest extends CakeTestCase {
+
+/**
+ * Suite define the tests for this suite
+ *
+ * @return void
+ */
+  public static function suite() {
+    $suite = new CakeTestSuite('All CsvView test');
+
+    $path = CakePlugin::path('CsvView') . 'Test' . DS . 'Case' . DS;
+    $suite->addTestDirectoryRecursive($path);
+
+    return $suite;
+  }
+}
