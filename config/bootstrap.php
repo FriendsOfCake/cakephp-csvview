4ae050128075f8a0c0e68f1f5bbd218f66fcd953
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2016-07-17 21:00:35 -0400

    Fix tests for EventManager

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,12 +1,17 @@
 <?php
 use Cake\Event\EventManager;
+use Cake\Event\Event;
 use Cake\Network\Request;
 
-EventManager::instance()->on('Controller.initialize', function (Cake\Event\Event $event) {
-  $controller = $event->subject();
-  if ($controller->components()->has('RequestHandler')) {
-    $controller->RequestHandler->config('viewClassMap.csv', 'CsvView.Csv');
-  }
+EventManager::instance()->on('Controller.initialize', function (Event $event) {
+    $controller = $event->subject();
+    if ($controller->components()->has('RequestHandler')) {
+        $controller->RequestHandler->config('viewClassMap.csv', 'CsvView.Csv');
+    }
 });
 
-Request::addDetector('csv', ['accept' => ['text/csv'], 'param' => '_ext', 'value' => 'csv']);
+Request::addDetector('csv', [
+    'accept' => ['text/csv'],
+    'param' => '_ext',
+    'value' => 'csv',
+]);
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -24,12 +24,10 @@ $root = $findRoot(__FILE__);
 unset($findRoot);
 
 chdir($root);
+require $root . '/vendor/cakephp/cakephp/tests/bootstrap.php';
 if (file_exists($root . '/config/bootstrap.php')) {
     require $root . '/config/bootstrap.php';
-    return;
 }
-
-require $root . '/vendor/cakephp/cakephp/tests/bootstrap.php';
 
 Configure::write('App', [
     'paths' => [
