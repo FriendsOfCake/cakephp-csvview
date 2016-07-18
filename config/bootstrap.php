ae87375351e9f831d9e491458a3eb72fc9913d6f
Jose Diaz-Gonzalez <josegonzalez@users.noreply.github.com>
2016-07-17 20:47:34 -0400

    Ensure we properly configure the CsvView classMap entry

---
+++ config/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+use Cake\Event\EventManager;
+
+EventManager::instance()->on('Controller.initialize', function (Cake\Event\Event $event) {
+  $controller = $event->subject();
+  if ($controller->components()->has('RequestHandler')) {
+    $controller->RequestHandler->config('viewClassMap.csv', 'CsvView.Csv');
+  }
+});
