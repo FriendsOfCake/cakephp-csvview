7c2e0d11994e5242a667ba99f87b10f26b6ddbef
Yevgeny Tomenko <skie@mail.ru>
2019-04-21 00:34:09 +0300

    add plugin file

---
+++ src/Plugin.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace CsvView;
+
+use Cake\Core\BasePlugin;
+use Cake\Core\PluginApplicationInterface;
+
+class Plugin extends BasePlugin
+{
+    /**
+     * Plugin name.
+     *
+     * @var string
+     */
+    protected $name = 'CsvView';
+}
